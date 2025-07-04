0a6feb26890503db60ade16613856a33d0de437b
Cadmus11 <obsidian.north.0013@gmail.com>
2025-07-04 11:14:23 +0000

    new anime app

--- src/UI/index.js
+++ src/UI/index.js
@@ -1,9 +1,9 @@
 import Header from "./Header";
 import AnimeList from './AnimeList'
 import Homepage from "./Homepage";
-import MovieBox from './MovieBox'
+
 
 
 export{
-    Header,MovieBox,AnimeList,Homepage
+    Header,AnimeList,Homepage
 }
